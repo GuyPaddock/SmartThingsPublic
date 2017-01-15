e487fa2c87bbdf5d88154d6c9ae3b7987000ad3c
Guy Paddock <guy.paddock@gmail.com>
2017-01-14 20:09:26 -0500

    Added support for binding to a routine.

--- smartapps/guypaddock/color-temperature-switcher.src/color-temperature-switcher.groovy
+++ smartapps/guypaddock/color-temperature-switcher.src/color-temperature-switcher.groovy
@@ -1,5 +1,7 @@
 /**
- *  Guy&#39;s First App
+ *  Color Temperature Switcher
+ *
+ *  Provides a way to force one or more bulbs to a certain color temperature when a certain routine is executed.
  *
  *  Copyright 2017 Guy Paddock
  *
@@ -13,59 +15,107 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+//==================================================================================================================
+// SmartThings Standard Definitions
+//==================================================================================================================
 definition(
-    name: "Color Temperature Switcher",
-    namespace: "GuyPaddock",
-    author: "Guy Paddock",
-    description: "Provides fine-grained control over the color temperature of one or more bulbs.",
-    category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+  name: "Color Temperature Switcher",
+  namespace: "GuyPaddock",
+  author: "Guy Paddock",
+  description: "Provides a way to force one or more bulbs to a certain color temperature when a certain routine is executed.",
+  category: "Convenience",
+  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+  iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+  iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
-	section("Configuration") {
-    	input name: "temperature", type: "number", title: "Temperature", description: "Desired color temperature", range: "1700..9500", required: true, displayDuringSetup: true
-    	input "bulbs", "capability.colorTemperature", multiple: true
-	}
+  page(name: "configurationPage")
 }
 
-def installed() {
-	log.debug "Installed with settings: ${settings}"
+//==================================================================================================================
+// Configuration Pages
+//==================================================================================================================
+def configurationPage() {
+  dynamicPage(name: "configurationPage", title: "", install: true, uninstall: true) {
+    section {
+       input name: "routine", type: "enum", title: "Which routine triggers this?", options: getAllRoutines()
+       input name: "temperature", type: "number", title: "What color temperature?", description: "Desired color temperature", range: "1700..9500", required: true, displayDuringSetup: true
+       input "bulbs", "capability.colorTemperature", multiple: true, title: "Which bulbs should be affected?"
+    }
+  }
+}
 
-	initialize()
+//==================================================================================================================
+// SmartApps Standard Events
+//==================================================================================================================
+def installed() {
+  log.debug "Installed with settings: ${settings}"
+
+  initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+  log.debug "Updated with settings: ${settings}"
 
-	unsubscribe()
-	initialize()
+  unsubscribe()
+  initialize()
 }
 
 def initialize() {
-	updateBulbs()
+  subscribe(location, "routineExecuted", routineChanged)
+    
+  log.debug "Subscribed to routine: ${routine}"
 }
 
-def updateBulbs() {
-	log.debug("Changing color temperature of all bulbs to: ${temperature}")
+//==================================================================================================================
+// Registered Event Callbacks
+//==================================================================================================================
+def routineChanged(changeEvent) {
+  log.trace "event name: ${changeEvent.name}"
+  log.trace "event value: ${changeEvent.value}"
+  log.trace "event displayName: ${changeEvent.displayName}"
+  log.trace "event descriptionText: ${changeEvent.descriptionText}"
 
-	printBulbsStatus();
-
-	for (bulb in bulbs) {
-		bulb.setColorTemperature(temperature)
-    }
-    
-	runIn(2, printBulbsStatus)
+  if (changeEvent.displayName == routine) {
+    log.debug "Selected routine has been triggered. Updating bulbs."
+    updateAllBulbs()
+  }
 }
 
-def printBulbsStatus() {
-	log.debug("Status of all bulbs is as follows:")
-    
-	for (bulb in bulbs) {
-        log.debug("  - ${bulb.currentState("colorTemperature").getValue()}")
-    }
+//==================================================================================================================
+// Protected API
+//==================================================================================================================
+def updateAllBulbs() {
+  log.debug "Changing color temperature of all bulbs to: ${temperature}"
 
-	log.debug("")
+  printStatusOfAllBulbs()
+
+  for (bulb in bulbs) {
+    bulb.setColorTemperature(temperature)
+  }
+    
+  runIn(2, printStatusOfAllBulbs)
+}
+
+def printStatusOfAllBulbs() {
+  log.debug "Status of all bulbs is as follows:"
+    
+  for (bulb in bulbs) {
+    log.debug "  - ${bulb.currentState("colorTemperature").getValue()}"
+  }
+
+  log.debug("")
+}
+
+def getAllRoutines() {
+  def result  = [];
+  def routines = location?.helloHome?.getPhrases()*.label
+
+  if (routines) {
+    routines.sort()
+    result = routines
+  }
+
+  return routines
 }
